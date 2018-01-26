bc77e88a9a2f5a85336a0742a3709fa0ead29517
horror-vacui <tzg6sa@gmail.com>
2018-01-26 23:42:25 +0100

    First commit: bash script toggling the touchpad enable/disable

---
+++ toggle_touchpad.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#MY_TOUCHPAD_ID = 14
+MY_TOUCHPAD_ID=$(xinput | grep -i "touch" | sed "s/.*id=\([0-9]\+\).*/\1/")
+
+a=$(xinput --list-props 14 | grep "Device Enabled" | cut -f 2 -d ":" | sed "s/[^0-9]*//g")
+
+# Safety: if the value is different from 0 or 1, then the touchpad will be enabled. 
+if [[ $a != 1 ]]; 
+	then xinput --enable $MY_TOUCHPAD_ID;
+	else xinput --disable $MY_TOUCHPAD_ID;
+fi;
+
