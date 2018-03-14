833833d5dc83dd4892081c53a631dcf60f41a927
Horror-Vacui <tzg6sa@gmail.com>
2018-03-14 15:54:30 +0100

    helper functions for key bindings

---
+++ search_and_create.sh
@@ -0,0 +1,41 @@
+# Bash functions to make window active or open applications
+# - firefox
+# - thunderbird
+# - anki
+# - toggle
+
+function search_open_firefox {
+  xdotool search "Mozilla Firefox" 
+  if [ $? -eq 0 ]; then
+    xdotool search "Mozilla Firefox" windowactivate 
+  else
+    firefox  
+  fi  
+}
+
+function search_open_anki {
+  xdotool search "Anki" 
+  if [ $? -eq 0 ]; then
+    xdotool search "Anki" windowactivate 
+  else
+    anki
+  fi  
+}
+
+function search_open_thunderbird {
+  xdotool search "Thunderbird" 
+  if [ $? -eq 0 ]; then
+    xdotool search "Thunderbird" windowactivate 
+  else
+    thunderbird
+  fi  
+}
+
+function search_open_toggl {
+  xdotool search "Toggl Desktop" 
+  if [ $? -eq 0 ]; then
+    xdotool search "Toggl Desktop" windowactivate 
+  else
+   /usr/local/toggl/TogglDesktop.sh 
+  fi  
+}
